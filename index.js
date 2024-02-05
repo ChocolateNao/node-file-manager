c01b1c94e7a18f764477da108f28369c9126c4e3
Andrey Loginov <andrey12q112@gmail.com>
2024-02-05 22:59:01 +0300

    feat: add working dir, colorize messages

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-import { exit } from "./src/handlers/handleExit.js";
-import { parseArgs } from "./src/utils/args/parseArgs.js";
+import os from 'node:os'
+
 import { createInterface } from "node:readline/promises";
+
+import { handleExit } from "./src/handlers/handleExit.js";
+import { parseArgs } from "./src/utils/args/parseArgs.js";
 import { handleCommand } from "./src/handlers/handleCommand.js"
+import colors from './src/utils/cliColors.js';
 
 export let args = {};
-
 if (process.argv.length > 2) {
   const argsArr = process.argv.slice(2);
   args = parseArgs(argsArr);
 }
+
+let initialDir = os.homedir();
+export let workingDir = initialDir;
+
+export const setWorkingDir = (dir) => {
+  workingDir = dir;
+  console.log(colors.lightCyan(`You are currently in ${workingDir}`));
+};
 
 export const rl = createInterface({
   input: process.stdin,
@@ -16,15 +27,15 @@ export const rl = createInterface({
 });
 
 const start = async (args) => {
-  console.log(`Welcome to the File Manager, ${args.username ? args.username : 'stranger'}!`);
+  console.log(colors.yellow(`Welcome to the File Manager, ${args.username ? args.username : 'stranger'}!`));
+  console.log(colors.lightCyan(`You are currently in ${initialDir}`));
 
   for await (const line of rl) {
     if (line !== '') {
       handleCommand(line);
     }
   }
-
-  exit();
+  handleExit();
 }
 
 start(args);
