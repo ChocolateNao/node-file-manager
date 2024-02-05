67ade6f0c650c7c86ca1f88b2fd39cbf58a55bb5
Andrey Loginov <andrey12q112@gmail.com>
2024-02-05 22:58:29 +0300

    feat: add main command handler

--- src/handlers/handleCommand.js
+++ src/handlers/handleCommand.js
@@ -1,21 +1,54 @@
-import { parseArgs } from "../utils/args/parseArgs.js";
-import { rl } from "../../index.js";
-import { exit } from "./handleExit.js";
+import { handleExit } from "./handleExit.js";
+import { handleOs } from "./handleOs.js";
+import { add, cat, cd, cp, ls, mv, rm, rn, up } from "../commands/filesystem.js";
+import colors from "../utils/cliColors.js";
+import { workingDir } from '../../index.js';
+import { hash } from "../commands/hash.js";
+import { compress, decompress } from "../commands/zip.js";
 
+const navigationCmd = ['up', 'cd']
+
+const handleWrongCommand = () => {
+  console.error(colors.red('Invalid input'));
+};
+
+const handleOperationFail = (msg) => {
+  console.error(msg ? colors.red(`Operation failed: ${msg.toString()}`) : colors.red('Operation failed'));
+  return;
+};
+
+const cmdSwitcher = {
+  'os': handleOs,
+  'up': up,
+  'cd': cd,
+  'ls': ls,
+  'cat': cat,
+  'add': add,
+  'rn': rn,
+  'cp': cp,
+  'mv': mv,
+  'rm': rm,
+  'hash': hash,
+  'compress': compress,
+  'decompress': decompress,
+  '.exit': handleExit,
+  'default': handleWrongCommand
+};
+
+/**
+ * Invokes after each end of input
+ */
 const handleCommand = (input) => {
-  const args = parseArgs(input.split(' '));
-  switch (input) {
-    case '.exit':
-      rl.pause();
-      exit();
-      break;
-    case input.startsWith('os'):
-      console.log("os reached");
-      break;
-    default:
-      console.log(`Got: ${input}`);
-      break;
+  if (input) {
+    const inputArr = input.split(' ');
+    const cmd = inputArr[0];
+    let args = inputArr.length === 1 ? '' : inputArr.slice(1);
+    (cmdSwitcher[cmd] || cmdSwitcher['default'])(args ?? null);
+
+    if (!navigationCmd.includes(cmd)) {
+      console.log(colors.lightCyan(`You are currently in ${workingDir}`));
+    }
   }
 };
 
-export { handleCommand };
+export { handleCommand, handleWrongCommand, handleOperationFail };
