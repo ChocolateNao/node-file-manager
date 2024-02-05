1fd7c2f94968417d74b5e9e6fc83a95d384951aa
ChocolateNao <andrey12q112@gmail.com>
2024-02-05 14:31:00 +0300

    feat: add rl interface

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-import { parseArgs } from "./src/utils/argsParser.js";
+import { exit } from "./src/handlers/handleExit.js";
+import { parseArgs } from "./src/utils/args/parseArgs.js";
+import { createInterface } from "node:readline/promises";
+import { handleCommand } from "./src/handlers/handleCommand.js"
 
-parseArgs()
-// console.log();
+export let args = {};
+
+if (process.argv.length > 2) {
+  const argsArr = process.argv.slice(2);
+  args = parseArgs(argsArr);
+}
+
+export const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const start = async (args) => {
+  console.log(`Welcome to the File Manager, ${args.username ? args.username : 'stranger'}!`);
+
+  for await (const line of rl) {
+    if (line !== '') {
+      handleCommand(line);
+    }
+  }
+
+  exit();
+}
+
+start(args);
